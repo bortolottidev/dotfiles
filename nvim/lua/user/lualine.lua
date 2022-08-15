ac97a4952d3a3947f32389584997d8d7166fb977
Daniele <29543986+bortolottidev@users.noreply.github.com>
2022-08-15 16:42:01 +0200

    Tuned lualine

--- nvim/lua/user/lualine.lua
+++ nvim/lua/user/lualine.lua
@@ -1,5 +1,5 @@
-local status_ok, lualine = pcall(require, "lualine")
-if not status_ok then
+local lualine = _SAFE_LOAD_PLUGIN("lualine")
+if not lualine then
 	return
 end
 
@@ -7,14 +7,25 @@ local hide_in_width = function()
 	return vim.fn.winwidth(0) > 80
 end
 
+local format_diagnostics = function(str)
+  local no_error_warning = string.len(str) == 0
+  -- show OK when theres no error or warning to show
+  if no_error_warning then
+    return ' Ok'
+  end
+
+  return str
+end
+
 local diagnostics = {
 	"diagnostics",
 	sources = { "nvim_diagnostic" },
 	sections = { "error", "warn" },
 	symbols = { error = " ", warn = " " },
-	colored = false,
+	colored = true,
 	update_in_insert = false,
-	always_visible = true,
+	always_visible = false,
+  fmt = format_diagnostics
 }
 
 local diff = {
@@ -27,14 +38,13 @@ local diff = {
 local mode = {
 	"mode",
 	fmt = function(str)
-		return "-- " .. str .. " --"
+		return str
 	end,
 }
 
 local filetype = {
 	"filetype",
-	icons_enabled = false,
-	icon = nil,
+	icons_enabled = true,
 }
 
 local branch = {
@@ -43,52 +53,32 @@ local branch = {
 	icon = "",
 }
 
-local location = {
-	"location",
-	padding = 0,
-}
-
--- cool function for progress
-local progress = function()
-	local current_line = vim.fn.line(".")
-	local total_lines = vim.fn.line("$")
-	local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
-	local line_ratio = current_line / total_lines
-	local index = math.ceil(line_ratio * #chars)
-	return chars[index]
-end
-
-local spaces = function()
-	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
-end
-
 lualine.setup({
 	options = {
 		icons_enabled = true,
     -- require tokyonight theme installed
 		theme = "tokyonight",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
 		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
 		always_divide_middle = true,
 	},
-	sections = {
-		lualine_a = { branch, diagnostics },
-		lualine_b = { mode },
-		lualine_c = {},
-		-- lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_x = { diff, spaces, "encoding", filetype },
-		lualine_y = { location },
-		lualine_z = { progress },
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
-		lualine_y = {},
-		lualine_z = {},
-	},
+  sections = {
+    lualine_a = { mode },
+    lualine_b = { branch, diff},
+    lualine_c = { diagnostics },
+    lualine_x = {'encoding', 'fileformat', filetype},
+    lualine_y = {'progress'},
+    lualine_z = {'filename', 'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {}
+  },
 	tabline = {},
 	extensions = {},
 })
