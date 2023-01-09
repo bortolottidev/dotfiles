b543ae4751a4e7147881d30e411335c5979e1907
bortolotti.dev <29543986+bortolottidev@users.noreply.github.com>
2023-01-08 19:28:27 -0500

    Add lsp zero base config

--- nvim/init.lua
+++ nvim/init.lua
@@ -21,6 +21,28 @@ require "config.cmp"
 
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
+vim.diagnostic.config({
+  virtual_text = false, -- display warn without tap on diagnostic
+  signs = true, -- required to display flag
+  update_in_insert = false,
+  underline = true,
+  severity_sort = true,
+  float = {
+    focusable = false,
+    style = 'minimal',
+    border = 'rounded',
+    source = 'always',
+    prefix = '',
+    header = '',
+  },
+})
+-- lsp.configure('sumneko_lua', {
+--   settings = {
+--     diagnostic = {
+--       globals = { 'vim' }
+--     }
+--   }
+-- })
 lsp.setup()
 
 require "config.telescope"
